da79a4a5b8f19de6747e9a4bfe17b50b767e0329
Cheng Lin <82935360+project-list@users.noreply.github.com>
2021-04-21 16:20:48 +0800

    first commit

---
+++ scripts/cToolManager.js
@@ -0,0 +1,148 @@
+﻿var CToolManager = function () {
+
+    document.addEventListener('keydown', CToolManager.OnKeyPress);
+    document.addEventListener('keyup', CToolManager.OnKeyUp);
+	
+	CContext.glViewContainer.addEventListener('mousedown', CToolManager.OnMouseDown);
+    CContext.glViewContainer.addEventListener('mouseup', CToolManager.OnMouseUp);
+    CContext.glViewContainer.addEventListener('mousemove', CToolManager.OnMouseMove);
+    CContext.glViewContainer.addEventListener('dblclick', CToolManager.OnMouseDoubleClick);
+	CToolManager.lineMaterial = new THREE.LineBasicMaterial({
+    color: 0x0000ff
+	});
+	
+    CManipulationTool.InitTool();
+    CLandmarkTool.InitTool();
+}
+
+CToolManager.currentActiontype = 'ManipulateCamera';
+CToolManager.OnKeyPress = function (e) {
+    var keyCode = e.keyCode;
+    //if (keyCode == 17) {//ctrl
+    //    CToolManager.currentActiontype = 'ManipulateCamera';
+    //}
+    //else 
+    if (keyCode == 18) {//alt
+        CToolManager.currentActiontype = 'ManipulateMesh';
+    }
+    else if (keyCode == 66) {//reset camera key_b
+        CContext.viewer.ResetCamera();
+    }
+    // else if (keyCode == 68) {//key_d draw
+        // if (CToolManager.currentActiontype == 'VolumeDraw') {
+            // CToolManager.currentActiontype = 'ManipulateCamera'
+        // }
+        // else
+        // CToolManager.currentActiontype = 'VolumeDraw';
+    // }
+    else if (keyCode == 80) {//key_p picklandmark
+        if (CToolManager.currentActiontype == 'LandmarkPicking') {
+            CToolManager.currentActiontype = 'ManipulateCamera'
+        }
+        else
+            CToolManager.currentActiontype = 'LandmarkPicking';
+    }
+    else if (keyCode == 82) {//key_r toggle render style
+        CContext.scene.ToggleRenderVolumeDataStyle();
+    }
+    else if (keyCode == 84) {//key_t toggle camera
+        CContext.viewer.ToggleCamera();
+    }
+    else if (keyCode == 76) {//L
+        var fname = 'resources/LowerJaw.stl';
+        var loader = new THREE.STLLoader();
+        loader.load(fname, function (geometry) {
+
+            var material = new THREE.MeshPhongMaterial({ color: 0xffffff, specular: 0xaaaaaa, shininess: 0, wireframe: false, shading: THREE.FlatShading, side: THREE.DoubleSide });
+           
+            var mesh = new THREE.Mesh(geometry, material);
+            mesh.geometry.normalsNeedUpdate = true;
+            //mesh.position.set(0, -0.25, 0.6);
+            mesh.rotation.set(0, -Math.PI, 0);
+            mesh.castShadow = true;
+            mesh.receiveShadow = true;
+			mesh.name="main_model";
+            CContext.scene.AddMeshObject(mesh);
+
+        });
+    }
+	else if (keyCode == 68) {//D
+		if (CToolManager.currentActiontype == 'DrawSketch') {
+            CToolManager.currentActiontype = 'ManipulateCamera'
+        }
+        else
+            CToolManager.currentActiontype = 'DrawSketch';
+	}
+	
+}
+CToolManager.OnKeyUp = function (e) {
+    var keyCode = e.keyCode;
+    //if (keyCode == 17) {//ctrl
+    //    CToolManager.currentActiontype = 'ManipulateCamera';
+    //}
+    //else 
+    if (keyCode == 18) {//alt
+        CToolManager.currentActiontype = 'ManipulateCamera';
+    }
+}
+
+
+// CToolManager.OnMouseDown = function (e) {
+    // if (CToolManager.currentActiontype == 'CrownSeg') {
+        // var mousepos = new THREE.Vector2(e.clientX, e.clientY);
+       
+       
+        // if (e.button == 0) {//left button{
+            // CToolManager.isLButtonDown = true;
+            // var glpos = CToolManager.Mouse2GlPos(mousepos);
+            // if (CToolManager.line != undefined)
+            // {
+                // CContext.scene.RemoveAuxObject(CToolManager.line);
+            // }
+            // CToolManager.lineGeo = new THREE.Geometry();
+            // CToolManager.lineMousePos= new Array();
+            // CToolManager.line = new THREE.Line(CToolManager.lineGeo.clone(), CToolManager.lineMaterial);
+
+        // }
+   
+    // }
+// }
+// CToolManager.Mouse2GlProjPos = function (mousePos) {
+    // var glprojpos = new THREE.Vector2();
+    // glprojpos.x = ((mousePos.x - CContext.glViewContainer.offsetLeft) / CContext.glViewContainer.offsetWidth) * 2 - 1;
+    // glprojpos.y = -((mousePos.y - CContext.glViewContainer.offsetTop) / CContext.glViewContainer.offsetHeight) * 2 + 1;
+    // return glprojpos;
+// }
+// CToolManager.Mouse2GlPos = function (mousePos) {
+    // var glprojpos = new THREE.Vector3();
+    // glprojpos.x = ((mousePos.x - CContext.glViewContainer.offsetLeft) / CContext.glViewContainer.offsetWidth) * 2 - 1;
+    // glprojpos.y = -((mousePos.y - CContext.glViewContainer.offsetTop) / CContext.glViewContainer.offsetHeight) * 2 + 1;
+    // glprojpos.z = -0.9;
+    // var glpos = new THREE.Vector3(glprojpos.x, glprojpos.y, glprojpos.z).unproject(CContext.viewer.camera);
+
+    // return glpos;
+// }
+// CToolManager.OnMouseMove = function (e) {
+    // if (CToolManager.currentActiontype == 'CrownSeg') {
+        // var mousepos = new THREE.Vector2(e.clientX, e.clientY);
+        // if (CToolManager.isLButtonDown && CToolManager.line != undefined) {
+            // var glpos = CToolManager.Mouse2GlPos(mousepos);
+ 
+            // CToolManager.lineGeo.vertices.push(glpos);
+            // CToolManager.lineMousePos.push(mousepos);
+            // if (CToolManager.lineGeo.vertices.length >= 2) {
+                // CContext.scene.RemoveAuxObject(CToolManager.line);
+                // CToolManager.line = new THREE.Line(CToolManager.lineGeo.clone(), CToolManager.lineMaterial);
+                // CContext.scene.AddAuxObject(CToolManager.line);
+            // }
+        // }
+    // }
+// }
+// CToolManager.OnMouseUp = function (e) {
+    // if (CToolManager.currentActiontype == 'CrownSeg') {
+        // if (e.button == 0) {//left button{
+            // CToolManager.isLButtonDown = false;
+        // }
+      
+    // }
+// }
---
+++ scripts/tools/cLandmarkTool.js
@@ -0,0 +1,245 @@
+﻿CLandmarkTool= function () {
+    CContext.glViewContainer.addEventListener('mousedown', CLandmarkTool.OnMouseDown);
+    CContext.glViewContainer.addEventListener('mouseup', CLandmarkTool.OnMouseUp);
+    CContext.glViewContainer.addEventListener('mousemove', CLandmarkTool.OnMouseMove);
+}
+CLandmarkTool.isLButtonDown = false;
+CLandmarkTool.isRButtonDown = false;
+CLandmarkTool.lineMaterial = new THREE.LineBasicMaterial({ color: 00000000, linewidth: 100.0} );
+CLandmarkTool.InitTool = function () {
+    CLandmarkTool();
+  
+CLandmarkTool.count=1;
+}
+
+
+CLandmarkTool.PickLandmark = function (mousepos) {
+
+    if (CLandmarkTool.isLButtonDown) {
+        var glMousePos = CManipulationTool.Mouse2GlProjPos(mousepos);
+        var meshArray = CContext.scene.GetMeshArray();
+        var intersects = CContext.scene.PickMeshs(glMousePos, CContext.viewer.camera);
+        var minDis = Number.MAX_VALUE;
+        var mi = -1;
+        for (var i = 0; i < intersects.length; i++) {
+            if (minDis > intersects[i].distance) {
+                minDis = intersects[i].distance;
+                mi = i;
+            }
+        }
+
+        if (mi == -1)
+            CContext.selectedMesh = null;
+        else {
+            // CContext.selectedMesh = intersects[mi].object;
+            // CContext.selectedMesh.material.color.set(0xdddd00);
+            var geometry = new THREE.SphereGeometry(0.01, 20, 20, 0, Math.PI * 2, 0, Math.PI * 2);
+            var material = new THREE.MeshPhongMaterial({ color: 0x0000ff, specular: 0xaaaaaa, shininess: 0, wireframe: false, shading: THREE.SmoothShading, side: THREE.DoubleSide });
+            var sphere = new THREE.Mesh(geometry, material);
+            sphere.position.set(intersects[mi].point.x, intersects[mi].point.y, intersects[mi].point.z);
+            sphere.updateMatrix();
+            sphere.type = 'surfacelandmark';
+			sphere.name = CLandmarkTool.count.toString();
+            CContext.scene.AddMeshObject(sphere);
+			setTimeout(input,300); 
+
+        }
+    }
+    else if (CLandmarkTool.isRButtonDown) {
+		var glMousePos = CManipulationTool.Mouse2GlProjPos(mousepos);
+        var intersects = CContext.scene.PickMeshs(glMousePos, CContext.viewer.camera);
+        var minDis = Number.MAX_VALUE;
+        var mi = -1;
+        for (var i = 0; i < intersects.length; i++) {
+            if (minDis > intersects[i].distance) {
+                minDis = intersects[i].distance;
+                mi = i;
+            }
+        }
+
+        if (mi == -1)
+            CContext.selectedMesh = null;
+        else {
+            CContext.selectedMesh = intersects[mi].object;
+			
+			if(CContext.selectedMesh.name=="main_model")
+				return;
+			
+            CContext.selectedMesh.material.color.set(0xdddd00);
+			name = CContext.selectedMesh.name;
+			
+			document.getElementById(name).style.background = "#FF6666";
+			for(var i=1; i<CLandmarkTool.count; i++)
+			{
+			obj_reset = i.toString();
+			if(obj_reset!=name)
+				{
+					document.getElementById(obj_reset).style.background = "#f0f8ff";
+					reset_object = CContext.scene.meshScene.getObjectByName(obj_reset);
+					reset_object.material.color.set(0x0000ff);
+				}
+			}
+    }
+	}
+
+}
+
+CLandmarkTool.Mouse2GlProjPos = function (mousePos) {
+    var glprojpos = new THREE.Vector2();
+    glprojpos.x = ((mousePos.x - CContext.glViewContainer.offsetLeft) / CContext.glViewContainer.offsetWidth) * 2 - 1;
+    glprojpos.y = -((mousePos.y - CContext.glViewContainer.offsetTop) / CContext.glViewContainer.offsetHeight) * 2 + 1;
+    return glprojpos;
+}
+CLandmarkTool.Mouse2GlPos = function (mousePos) {
+    var glprojpos = new THREE.Vector3();
+    glprojpos.x = ((mousePos.x - CContext.glViewContainer.offsetLeft) / CContext.glViewContainer.offsetWidth) * 2 - 1;
+    glprojpos.y = -((mousePos.y - CContext.glViewContainer.offsetTop) / CContext.glViewContainer.offsetHeight) * 2 + 1;
+    glprojpos.z = -0.9;
+    var glpos = new THREE.Vector3(glprojpos.x, glprojpos.y, glprojpos.z).unproject(CContext.viewer.camera);
+
+    return glpos;
+}
+
+function makeLine(geo, name) {
+        var g = new MeshLine();
+        g.setGeometry(geo);
+    
+        var material = new MeshLineMaterial( {
+            useMap: false,
+            color: new THREE.Color( 0x0000ff ),
+            opacity: 1,
+            resolution: new THREE.Vector2( window.innerWidth, window.innerHeight ),
+            sizeAttenuation: !false,
+            lineWidth: 0.01
+        });
+        mesh = new THREE.Mesh( g.geometry, material );
+		mesh.name = name;
+		//mesh.raycast = MeshLineRaycast;
+        CContext.scene.AddAuxObject( mesh );
+}
+
+CLandmarkTool.OnMouseDown = function (e) {
+    if (CToolManager.currentActiontype == 'LandmarkPicking') {
+        var mousepos = new THREE.Vector2(e.clientX, e.clientY);
+        var glpos = CManipulationTool.Mouse2GlProjPos(mousepos);
+        if (event.button == 0) {//left button{
+            CLandmarkTool.isLButtonDown = true;
+            CLandmarkTool.PickLandmark(mousepos);
+
+        }
+        else if (event.button == 2) {//right button{
+            CLandmarkTool.isRButtonDown = true;
+            CLandmarkTool.PickLandmark(mousepos);
+
+        }
+    }
+	else if (CToolManager.currentActiontype == 'DrawSketch')
+	{
+		var mousepos = new THREE.Vector2(e.clientX, e.clientY);
+
+        if (e.button == 0) {//left button{
+            CLandmarkTool.isLButtonDown = true;
+            CLandmarkTool.lineGeo = new THREE.Geometry();
+            CLandmarkTool.lineMousePos= new Array();
+            CLandmarkTool.line = new THREE.Line(CLandmarkTool.lineGeo.clone(), CLandmarkTool.lineMaterial);
+			
+			CLandmarkTool.lineGeo2d = new THREE.Geometry();
+			CLandmarkTool.line2d = new THREE.Line(CLandmarkTool.lineGeo2d.clone(), CLandmarkTool.lineMaterial);
+
+			}
+			
+		if (e.button == 2) {//left button{
+            CLandmarkTool.isRButtonDown = true;
+			}
+	}
+	
+	
+}
+
+
+CLandmarkTool.OnMouseMove = function (e) {
+    if (CToolManager.currentActiontype == 'LandmarkPicking') {
+        var mousepos = new THREE.Vector2(e.clientX, e.clientY);
+        if (CLandmarkTool.isLButtonDown) {
+           
+        }
+    }
+	else if (CToolManager.currentActiontype == 'DrawSketch') {
+		
+	    var mousepos = new THREE.Vector2(e.clientX, e.clientY);
+		var glpos2d = CLandmarkTool.Mouse2GlPos(mousepos);
+
+		if (CLandmarkTool.isLButtonDown) {
+			{
+				CLandmarkTool.lineMousePos.push(mousepos);
+				CLandmarkTool.lineGeo2d.vertices.push(glpos2d);
+				
+				if (CLandmarkTool.lineGeo2d.vertices.length >= 2) 
+				{
+				CLandmarkTool.line2d = new THREE.Line(CLandmarkTool.lineGeo2d.clone(), CLandmarkTool.lineMaterial);
+                //CContext.scene.AddAuxObject(CLandmarkTool.line2d);
+				}
+			}
+		}
+        
+		}
+	
+}
+
+
+function input(){
+		var comment=prompt("Please input your comments:");
+		var id=(CLandmarkTool.count++).toString();
+		document.getElementById("right").innerHTML+= "<div id =\"" + id +  "\" style = \"font-size: 35px; width:95%; margin: auto; background: #f0f8ff; border-radius:15px \" > comments " 
+		+ id + ":<br>" + comment + "</div><br>";
+} 
+
+CLandmarkTool.OnMouseUp = function (e) {
+    if (CToolManager.currentActiontype == 'LandmarkPicking') {
+        if (event.button == 0) {//left button{
+            CLandmarkTool.isLButtonDown = false;
+        }
+        else if (event.button ==2) {//right button{
+            CLandmarkTool.isRButtonDown = false;
+        }
+    }
+	
+	if (CToolManager.currentActiontype == 'DrawSketch') 
+	{	
+        if (event.button == 0) {//left button{
+            CLandmarkTool.isLButtonDown = false;
+        }
+        else if (event.button ==2) {//right button{
+            CLandmarkTool.isRButtonDown = false;
+        }
+		
+		for (var k=0; k<CLandmarkTool.lineMousePos.length; k++)
+		{	
+			var mousepos = CLandmarkTool.lineMousePos[k];
+			var glMousePos = CManipulationTool.Mouse2GlProjPos(mousepos);
+			var intersects = CContext.scene.PickMeshs(glMousePos, CContext.viewer.camera);
+			var minDis = Number.MAX_VALUE;
+			var mi = -1;
+			for (var i = 0; i < intersects.length; i+=10) {
+				if (minDis > intersects[i].distance) {
+					minDis = intersects[i].distance;
+					mi = i;
+				}
+			}
+			if (mi == -1)
+				CContext.selectedMesh = null;
+			else {
+				CLandmarkTool.lineGeo.vertices.push(new THREE.Vector3(intersects[mi].point.x, intersects[mi].point.y, intersects[mi].point.z));
+			}
+			
+		}
+		if (CLandmarkTool.lineGeo.vertices.length >= 2) {
+
+			CLandmarkTool.line = new THREE.Line(CLandmarkTool.lineGeo.clone(), CLandmarkTool.lineMaterial);
+			makeLine(CLandmarkTool.lineGeo.clone(), CLandmarkTool.count.toString());
+			// CContext.scene.AddAuxObject(CLandmarkTool.line);
+		}
+		if (CLandmarkTool.lineGeo.vertices.length >= 2)
+		setTimeout(input, CLandmarkTool.count.toString()); 
+	}
+}
